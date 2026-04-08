f078ddf99441ce34b77dbdf2343615380ae6f181
Andres Nieves <andresnieves2425@gmail.com>
2026-04-08 15:56:47 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -8,27 +8,16 @@ One function per operation, in order.
 import math
 def add(a, b): a + b
 
-def sub(a, b): a - b
+def subtract(a, b): a - b
 
 def mul(a, b): a * b
 
 def div(a, b): b / a # raise ZeroDivisionError if a == 0
 
-def log(a, b): loga(b)# use math library + raise ValueError
-
 def exp(a, b): ab
-import math
 
 def square_root(a): math.sqrt(a)# raise ValueError if a < 0
 
 def hypotenuse(a, b): math.hypot(a, b) # can have negative nums
 
-def add(a, b): a + b
-
-def subtract(a, b): a - b
-
-def multiply(a, b): a * b
-
 def logarithm(a, b): loga(b)# use math library/raise ValueError
-
-def exponent(a, b): ab
